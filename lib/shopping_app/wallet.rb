d0054649b010adb33e8ecedbceae42627f9220a2
Yohei Goto <yohei_goto@diveintocode.jp>
2020-11-29 20:11:56 +0900

    :skull: private method

--- lib/shopping_app/wallet.rb
+++ lib/shopping_app/wallet.rb
@@ -11,19 +11,9 @@ class Wallet
   end
 
   def withdraw(amount)
-    return unless enough_balance?(amount)
+    return unless @balance >= amount
     @balance -= amount.to_i
     amount
   end
 
-  private
-
-  def enough_balance?(amount)
-    if @balance >= amount
-      true
-    else
-      puts "ウォレットに十分な残高がありません"
-      false
-    end
-  end
 end
